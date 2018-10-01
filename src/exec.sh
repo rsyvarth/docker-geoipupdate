e5c58ad41e20903548d59a3434fee713e5d0e809
Robert Syvarth <robert.syvarth@gmail.com>
2018-10-01 15:51:52 -0700

    Fix compatibility with new versions of docker

--- src/exec.sh
+++ src/exec.sh
@@ -3,13 +3,13 @@
 # set a configuration file if not already set
 ! (: "${GEOIP_CONFIG_FILE?}") 2>/dev/null && {
     GEOIP_CONFIG_FILE="/usr/local/etc/GeoIP.conf"
-    [[ ! -z $GEOIP_USER_ID ]] && {
-        echo "UserId $GEOIP_USER_ID" > $GEOIP_CONFIG_FILE
+    [[ ! -z $GEOIP_ACCOUNT_ID ]] && {
+        echo "AccountID $GEOIP_ACCOUNT_ID" > $GEOIP_CONFIG_FILE
     }
     [[ ! -z $GEOIP_LICENSE_KEY ]] && {
         echo "LicenseKey $GEOIP_LICENSE_KEY" > $GEOIP_CONFIG_FILE
     }
-    echo "ProductIds ${GEOIP_PRODUCT_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
+    echo "EditionIDs ${GEOIP_EDITION_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
     echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/GeoIP"}" >> $GEOIP_CONFIG_FILE
 }
 
