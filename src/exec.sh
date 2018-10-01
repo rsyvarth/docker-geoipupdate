cec9aff0d72a19dc4271526b29c84955c3c5c685
Robert Syvarth <robert.syvarth@gmail.com>
2018-10-01 16:31:25 -0700

    Don't allow account id to be optional

--- src/exec.sh
+++ src/exec.sh
@@ -3,12 +3,8 @@
 # set a configuration file if not already set
 ! (: "${GEOIP_CONFIG_FILE?}") 2>/dev/null && {
     GEOIP_CONFIG_FILE="/usr/local/etc/maxmind-geoip.conf"
-    [[ ! -z $GEOIP_ACCOUNT_ID ]] && {
-        echo "AccountID $GEOIP_ACCOUNT_ID" > $GEOIP_CONFIG_FILE
-    }
-    [[ ! -z $GEOIP_LICENSE_KEY ]] && {
-        echo "LicenseKey $GEOIP_LICENSE_KEY" > $GEOIP_CONFIG_FILE
-    }
+    echo "AccountID $GEOIP_ACCOUNT_ID" >> $GEOIP_CONFIG_FILE
+    echo "LicenseKey $GEOIP_LICENSE_KEY" >> $GEOIP_CONFIG_FILE
     echo "EditionIDs ${GEOIP_EDITION_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
     echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/maxmind/"}" >> $GEOIP_CONFIG_FILE
 }
