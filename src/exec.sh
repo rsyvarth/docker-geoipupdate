723a94d229f686476648b466f15a688ac575dbba
Robert Syvarth <robert.syvarth@gmail.com>
2018-10-01 16:16:52 -0700

    Update config file path

--- src/exec.sh
+++ src/exec.sh
@@ -2,7 +2,7 @@
 
 # set a configuration file if not already set
 ! (: "${GEOIP_CONFIG_FILE?}") 2>/dev/null && {
-    GEOIP_CONFIG_FILE="/usr/local/etc/GeoIP.conf"
+    GEOIP_CONFIG_FILE="/usr/local/etc/maxmind-geoip.conf"
     [[ ! -z $GEOIP_ACCOUNT_ID ]] && {
         echo "AccountID $GEOIP_ACCOUNT_ID" > $GEOIP_CONFIG_FILE
     }
@@ -10,7 +10,7 @@
         echo "LicenseKey $GEOIP_LICENSE_KEY" > $GEOIP_CONFIG_FILE
     }
     echo "EditionIDs ${GEOIP_EDITION_IDS:-"GeoLite2-City GeoLite2-Country"}" >> $GEOIP_CONFIG_FILE
-    echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/GeoIP"}" >> $GEOIP_CONFIG_FILE
+    echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/maxmind/"}" >> $GEOIP_CONFIG_FILE
 }
 
 # execute the updates with verbose output
