ef538f511df6beb6d84ca7a01a3f7d5dbd1e623b
Robert Syvarth <robert.syvarth@gmail.com>
2018-10-01 16:37:22 -0700

    Remove config file cat

--- src/exec.sh
+++ src/exec.sh
@@ -9,7 +9,5 @@
     echo "DatabaseDirectory ${GEOIP_DIRECTORY:-"/usr/local/share/maxmind/"}" >> $GEOIP_CONFIG_FILE
 }
 
-cat $GEOIP_CONFIG_FILE
-
 # execute the updates with verbose output
 geoipupdate -f $GEOIP_CONFIG_FILE -v
